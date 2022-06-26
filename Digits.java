e5a98762ffa5fbf828ddf8bfd703781dcded6e2f
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2022-06-26 12:52:40 -0700

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,11 @@
+public class Digits{
+   public static void main (String [] args){
+   
+   int num=254;
+   
+   System.out.println((num/100));
+   System.out.println(((num/10)%10));
+   System.out.println( (num%10));
+   
+   }
+}
